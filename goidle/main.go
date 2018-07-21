65d5ef462a09393852875cc330af5ef14b528ee8
Lonnie Liu <liulonnie@gmail.com>
2018-07-20 23:32:44 -0700

    add goidle

---
+++ goidle/main.go
@@ -0,0 +1,13 @@
+// Command goidle runs forever but does nothing. This is useful to keep
+// a docker container in running state.
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	for {
+		time.Sleep(time.Hour)
+	}
+}
